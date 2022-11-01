31c6ffd98e566c3b4b91466775082ea876607dd9
Mike Rachman <mikerachman1@gmail.com>
2022-11-01 09:19:45 -0400

    filled out seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all 
+
+User.create!([{
+  email: "mike@aol.com",
+  encrypted_password: "password",
+  username: "mike"
+},
+{
+  email: "logan@aol.com",
+  encrypted_password: "password",
+  username: "logan"
+},
+{
+  email: "kendall@aol.com",
+  encrypted_password: "password",
+  username: "kendall"
+},
+{
+  email: "roman@aol.com",
+  encrypted_password: "password",
+  username: "roman"
+},
+{
+  email: "shiv@aol.com",
+  encrypted_password: "password",
+  username: "shiv"
+}])
