6d138dc70d3320e4d017a9424867d2c9838fc10d
Mike Rachman <mikerachman1@gmail.com>
2022-11-01 15:27:21 -0400

    added invitations controller

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -14,10 +14,10 @@ class Invitation < ApplicationRecord
   end
 
   def self.find_invitation(id1, id2)
-    if Invitation.where(user_id: id1, friend_id: id2, confirmed: true).empty?
-      Invitation.where(user_id: id2, friend_id: id1, confirmed: true)[0].id
+    if Invitation.where(user_id: id1, friend_id: id2).empty?
+      Invitation.where(user_id: id2, friend_id: id1)[0].id
     else
-      Invitation.where(user_id: id1, friend_id: id2, confirmed: true)[0].id
+      Invitation.where(user_id: id1, friend_id: id2)[0].id
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
 
-  resources :users
+  resources :users do
+    resources :invitations
+  end
 end
