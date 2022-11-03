c3190446604524a738700a66cc090e9b14aa85f1
Mike Rachman <mikerachman1@gmail.com>
2022-11-03 14:07:31 -0400

    added #destroy action to notifications controller and added "delete notification" link to notifications#index view

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,6 +6,13 @@ class NotificationsController < ApplicationController
     @notifications = @user.notifications
   end
 
+  def destroy
+    @user = User.find(params[:user_id])
+    @notification = @user.notifications.find(params[:id])
+    @notification.destroy
+    redirect_to user_notifications_path(@user)
+  end
+
   private
 
   def check_user(user)
