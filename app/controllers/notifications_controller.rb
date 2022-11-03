c6b604e54713911fafebe9b19fa467f555e5d41b
Mike Rachman <mikerachman1@gmail.com>
2022-11-03 13:59:40 -0400

    added before_action filter to notifications_controller so users cant view other users notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,17 @@
 class NotificationsController < ApplicationController
+  before_action -> { check_user(User.find(params[:user_id])) }
+  
   def index
     @user = User.find(params[:user_id])
     @notifications = @user.notifications
+  end
+
+  private
+
+  def check_user(user)
+    unless user == current_user
+      flash[:alert] = 'You can only view your own notifications'
+      redirect_to root_path
+    end
   end
 end
