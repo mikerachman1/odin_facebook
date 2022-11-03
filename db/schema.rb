dc9c67427ace9f2dd12c40898d4b85a74a4e5aa1
Mike Rachman <mikerachman1@gmail.com>
2022-11-03 11:47:19 -0400

    ran migration for notifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_01_124326) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_153744) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_124326) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_invitations_on_user_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "body"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +45,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_124326) do
   end
 
   add_foreign_key "invitations", "users"
+  add_foreign_key "notifications", "users"
 end
