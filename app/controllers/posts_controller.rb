e5885ef74f82cd6012ac2c9808de55f6e2f19267
Mike Rachman <mikerachman1@gmail.com>
2022-11-04 16:37:57 -0400

    wrote #index action of posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+  def index
+    #posts from current user and current users friends
+    @user = current_user
+    @friends = @user.friends
+    @posts = @user.posts
+    @friends.each do |friend|
+      @posts << friend.posts
+    end
+  end
 end
