db9c9583e783a47f29d9da527f0788223e2b0044
Mike Rachman <mikerachman1@gmail.com>
2022-11-06 12:51:37 -0500

    added timeline link to header nav, created new post form and put on posts#index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,24 @@
 class PostsController < ApplicationController
   def index
     #posts from current user and current users friends
-    @user = current_user
+    @user = User.find(params[:user_id])
     @friends = @user.friends
-    @posts = @user.posts
+    @posts = []
+    @user.posts.map { |post| @posts << post }
     @friends.each do |friend|
-      @posts << friend.posts
+      friend.posts.map { |post| @posts << post }
     end
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.create(post_params)
+    redirect_to user_posts_path(@user)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :user_id)
   end
 end
