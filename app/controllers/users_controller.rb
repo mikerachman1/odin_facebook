a1f694aa62733e8fc05956690cafa5fc50e41bb8
Mike Rachman <mikerachman1@gmail.com>
2022-11-06 13:11:56 -0500

    added users posts to user#show page, added new post form as well

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    unsorted_posts = @user.posts
+    @posts = unsorted_posts.sort_by(&:created_at).reverse
   end
 end
