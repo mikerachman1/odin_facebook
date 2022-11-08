006e45fb72800c936ab8d22286fd15939e1c7ebc
Mike Rachman <mikerachman1@gmail.com>
2022-11-08 14:26:05 -0500

    ran db migration and generated controller for comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_125141) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_08_192240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "invitations", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -61,6 +71,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_125141) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "invitations", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
