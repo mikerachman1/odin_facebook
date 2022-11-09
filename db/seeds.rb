6c04c28eff708235a00853121524ba7645a535a9
Mike Rachman <mikerachman1@gmail.com>
2022-11-09 14:17:02 -0500

    added letter_opener gem and generated User mailer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Comment.destroy_all
+Like.destroy_all
 Post.destroy_all
 Notification.destroy_all
 Invitation.destroy_all
