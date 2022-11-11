2a5e0ee6b0b710bcaed6b6394e04b86d31c3c863
Mike Rachman <mikerachman1@gmail.com>
2022-11-11 10:43:19 -0500

    added columns to user table for google sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         #for google Omniauth
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :invitations
   has_many :pending_invitations, -> { where confirmed: false }, class_name: 'Invitation', foreign_key: "friend_id"
